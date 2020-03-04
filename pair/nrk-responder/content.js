a4919504a20984d84c6c3ef8fd3f607ed6ace015
wolf3d <oldcar@inbox.lv>
2020-03-04 18:57:51 +0200

    latest progress

--- pair/nrk-responder/content.js
+++ pair/nrk-responder/content.js
@@ -6,18 +6,18 @@
 //     });
 // });
 
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//       console.log(request);
-//         //sendResponse({farewell: "goodbye"});
-//     });
-
-    var port = chrome.runtime.connect({name: "knockknock"});
-    //port.postMessage({joke: "Knock knock"});
-    port.onMessage.addListener(function(beskjed) {
-        console.log(beskjed);
-    //   if (msg.question == "Who's there?")
-    //     port.postMessage({answer: "Madame"});
-    //   else if (msg.question == "Madame who?")
-    //     port.postMessage({answer: "Madame... Bovary"});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(request);
+        //sendResponse({farewell: "goodbye"});
     });
+
+    // var port = chrome.runtime.connect({name: "knockknock"});
+    // //port.postMessage({joke: "Knock knock"});
+    // port.onMessage.addListener(function(beskjed) {
+    //     console.log(beskjed);
+    // //   if (msg.question == "Who's there?")
+    // //     port.postMessage({answer: "Madame"});
+    // //   else if (msg.question == "Madame who?")
+    // //     port.postMessage({answer: "Madame... Bovary"});
+    // });
