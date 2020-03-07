731ad05f32a42431f347bd11413ec560a95c9ee6
wolf3d <oldcar@inbox.lv>
2020-03-07 18:42:49 +0200

    translated captions sent back to nrk page

--- pair/nrk-responder/content.js
+++ pair/nrk-responder/content.js
@@ -13,15 +13,32 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 // Select the node that will be observed for mutations
 const targetNode = items[0];
 
+// The ID of the extension we want to talk to.                 
+// tracker extension id   
+var translatorExtensionId = "bkbhmoenlfibhnfakjbcbcaghegapgkd";
+
+//// Start a long-running conversation:
+var port = chrome.runtime.connect(translatorExtensionId);
+
 // Options for the observer (which mutations to observe)
 const config = { attributes: false, childList: true, subtree: true };
-
+var cmp = '';
 // Callback function to execute when mutations are observed
 const callback = function(mutationsList, observer) {
     // Use traditional 'for loops' for IE 11
     for(let mutation of mutationsList) {
         if (mutation.type === 'childList') {
             console.log('A child node has been added or removed.');
+            if (cmp != targetNode.innerText && targetNode.innerText.indexOf("....") == -1) {
+                cmp = targetNode.innerText;
+                console.log('innerText:' + cmp);
+                port.postMessage({
+                    en: cmp
+                });   
+            }            
+        }
+        if (mutation.type === 'subtree') {
+            console.log('subtree.');
         }
     }
 };
